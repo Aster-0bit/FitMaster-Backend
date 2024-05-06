47dcc8ee8660ec6e66df42ca98b1a3eb9da263a8
Aster-0bit <jonathan30012rioja@gmail.com>
2024-05-06 07:10:29 +0200

    Added new user schema

---
+++ schemas/users.js
@@ -0,0 +1,17 @@
+import z from 'zod'
+
+const userSchema = z.object({
+  name: z.string({
+    invalid_type_error: 'User name must be a string',
+    required_error: 'User name is requires'
+  }),
+  email: z.string().z.email({
+    message: 'Invalid email address'
+  }),
+  password: z.string().min(4),
+
+})
+
+export function validateUser (input) {
+  return userSchema.safeParse(input)
+}
