e4d427784f9908621c053e45b1d3ca95633abdf0
Aster-0bit <jonathan30012rioja@gmail.com>
2024-05-07 22:32:11 +0200

    Fixed user model

--- models/user.js
+++ models/user.js
@@ -1,12 +1,15 @@
 import mysql from 'mysql2/promise'
 
-const config = {
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT
-}
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+})
 
 const connection = await mysql.createConnection(config)
 
@@ -27,22 +30,14 @@ export class UserModel {
     } = input
 
     try {
-      const [result] = await connection.query(
+      const [result] = await pool.query(
         'INSERT INTO Users ( name, email, password ) VALUES (?, ?, ?);',
         [name, email, password]
       )
       const userId = result.insertId;  
       return { userId, message: 'User created successfully' };
     }catch (e) {
-      if (e.code === 'ER_DUP_ENTRY') {
-        res.status(409).send({ error: 'Email already exists.' });
-      } else {
-        res.status(500).send({ error: 'Error creating user. Please try again later.' });
-      }
-    } finally {
-      if (connection) {
-        connection.end();
-      }
+      throw e
     }
   }
 
