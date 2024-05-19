198dc56d0b72d2f91f9bc3954d5ae0d46800bbd8
Jonathan1209 <jonathan30012rioja@gmail.com>
2024-05-19 12:37:07 +0200

    Added new orgin in cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -4,6 +4,7 @@ const ACCEPTED_ORIGINS = [
   'http://localhost:8080',
   'http://localhost:1234',
   'http://localhost:5173',
+  'https://fit-master-frontend.vercel.app',
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
