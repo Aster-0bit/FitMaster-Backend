4144ad5d9553f4ded5e234c17837d601b83fb138
Aster-0bit <jonathanrioja38@gmail.com>
2024-05-31 07:50:50 +0200

    refactor: Add localhost:3000 to accepted origins in cors middleware

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,6 +2,7 @@ import cors from 'cors'
 
 const ACCEPTED_ORIGINS = [
   'http://localhost:8080',
+  'http://localhost:3000',
   'http://localhost:1234',
   'http://localhost:5173',
   'https://fit-master-frontend.vercel.app',
